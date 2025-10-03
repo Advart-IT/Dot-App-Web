36779514c31cf3f0f5211bc3071dfc9fbf08aaa6
Advart_It <data.advart@gmail.com>
2025-10-03 15:52:18 +0530

    done

--- lib/data/apiurl.tsx
+++ lib/data/apiurl.tsx
@@ -1,3 +1,3 @@
-// export const API_URL = process.env.API_URL || "https://data.advartit.in";
+export const API_URL = process.env.API_URL || "https://data.advartit.in";
 
-export const API_URL = process.env.API_URL || "http://localhost:9000" || "https://data.advartit.in"; // Fallback to localhost if not defined
+// export const API_URL = process.env.API_URL || "http://localhost:9000" || "https://data.advartit.in"; // Fallback to localhost if not defined
